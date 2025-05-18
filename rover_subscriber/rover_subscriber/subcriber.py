c1f8d64a20a323bf1b9e8cdf7fb73097a0b05736
Léonard Lemaire <leonard.lemaire@epfl.ch>
2025-05-18 12:41:18 +0200

    Implement new subscriber that supports autonomous driving mode

---
+++ rover_subscriber/rover_subscriber/subcriber.py
@@ -0,0 +1,291 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String
+import serial
+from math import sqrt
+
+WHEEL_DIAMETER_CM = 6.6
+TICKS_PER_REV = 20
+WHEEL_BASE_CM = 27.3
+
+class TrajectorySubscriber(Node):
+
+    def __init__(self):
+        super().__init__('trajectory_subscriber') #node name and instanciation
+        
+        self.subscription = self.create_subscription(
+            Joy, #receive Joy messages
+            '/cmd_vel', #topic name to subscribe to
+            self.listener_callback, #method to call when receiving a message
+            10)
+        
+        self.publisher = self.create_publisher(String, '/odom', 2)
+        
+        self.autonomous_mode = False
+        self.prev_fl = 0
+        self.prev_fr = 0
+        self.curr_rotation = 0
+        self.curr_distance = 0
+        self.curr_command = ""
+        self.timer = self.create_timer(0.2, self.read_serial)
+        
+        self.rf_direction = 0 #FRONT RIGHT WHEEL
+        self.rf_speed = 0
+        
+        self.lf_direction = 0 #FRONT LEFT WHEEL
+        self.lf_speed = 0
+        
+        self.rb_direction = 0 #BACK RIGHT WHEEL
+        self.rb_speed = 0
+        
+        self.lb_speed = 0 #BACK LEFT WHEEL
+        self.lb_direction = 0
+        
+        #Arduino master connected port
+        self.ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+        self.ser.reset_input_buffer()
+        self.get_logger().info('Subscriber node has been started.')
+        
+        
+    def read_serial(self):
+        try:
+            line = self.ser.readline().decode().strip()  # e.g., "FL:120,FR:122"
+            if not line.startswith("FL"):
+                return
+            fl_ticks = int(line.split(",")[0].split(":")[1])
+            fr_ticks = int(line.split(",")[1].split(":")[1])
+
+            # Compute deltas
+            delta_fl = fl_ticks - self.prev_fl
+            delta_fr = fr_ticks - self.prev_fr
+            self.prev_fl = fl_ticks
+            self.prev_fr = fr_ticks
+
+            # Compute distances
+            dist_fl = (delta_fl / TICKS_PER_REV) * 3.1416 * WHEEL_DIAMETER_CM
+            dist_fr = (delta_fr / TICKS_PER_REV) * 3.1416 * WHEEL_DIAMETER_CM
+            distance_cm = (dist_fl + dist_fr) / 2.0
+
+            # Compute rotation (angle in degrees)
+            angle_deg = ((dist_fr - dist_fl) / WHEEL_BASE_CM) * 180 / 3.1416
+
+            self.get_logger().info(f"Moved {distance_cm:.2f} cm, Rotated {angle_deg:.2f}°")
+            
+            if self.autonomous_mode:
+                self.curr_distance += distance_cm
+                self.curr_rotation += angle_deg
+                if self.curr_command == "up":
+                    if self.curr_distance >= 30:
+                        self.finish_step()
+                    else:
+                        self.set_ahead()
+                        self.set_same_speed(1)
+                        self.send_data()
+                elif self.curr_command == "down":
+                    if self.curr_distance <= -30:
+                        self.finish_step()
+                    else:
+                        self.set_backward()
+                        self.set_same_speed(1)
+                        self.send_data()
+                elif self.curr_command == "left":
+                    if self.curr_rotation >= 90:
+                        self.finish_step()
+                    else:
+                        self.set_left()
+                        self.set_same_speed(1)
+                        self.send_data()
+                elif self.curr_command == "right":
+                    if self.curr_rotation <= -90:
+                        self.finish_step()
+                    else:
+                        self.set_right()
+                        self.set_same_speed(1)
+                        self.send_data()
+                
+        except Exception as e:
+            self.get_logger().error(f"Error reading serial: {e}")
+
+    def listener_callback(self, msg):
+        buttons = msg.buttons
+
+        # Toggle mode (triangle = index 12)
+        if buttons[3] == 1 and self.prev_buttons[3] == 0:
+            self.autonomous_mode = not self.autonomous_mode
+            mode = "autonome" if self.autonomous_mode else "manuel"
+            self.get_logger().info(f"Mode changé : {mode}")
+        
+        if self.autonomous_mode:
+            if msg.axes[1] == 1:
+                self.curr_command == "up"
+            elif msg.axes[1] == -1:
+                self.curr_command == "down"
+            elif msg.axes[0] == 1:
+                self.curr_command == "left"
+            elif msg.axes[0] == -1:
+                self.curr_command == "right"
+        
+        x = msg.axes[0] # x-axis of left joystick
+        
+        #SAFETY BOUND FOR X
+        if x < -1:
+            x = -1
+        elif x > 1:
+            x = 1
+            
+        y = msg.axes[1] # y-axis of left joystick
+        
+        #SAFETY BOUND FOR Y
+        if y < -1:
+            y = -1
+        elif y > 1:
+            y = 1
+            
+        speed = sqrt(x**2 + y**2) #euclidean distance to the center
+        
+        #SAFETY BOUND FOR SPEED
+        if speed > 1:
+            speed = 1
+        
+        #DIRECTION ASSIGNMENT
+        if (x < 0 and y >= -0.2 * x) or (x >= 0 and y >= 0.2 * x):
+            self.set_ahead()
+        elif ((x < 0 and y <= 0.2 * x) or (x >= 0 and y <= -0.2 * x)):
+            self.set_backward()
+        elif x < 0:
+            self.set_right()
+        else:
+            self.set_left()
+            
+        #SPEED ASSIGNMENT
+        if ((x < 0 and y >= 0.2 * x and y <= -0.2 * x) #RIGHT SECTION 
+            or (x >= 0 and y <= 0.2 * x and y >= -0.2 * x) #LEFT SECTION
+            or ((x < 0 and y >= -5 * x) or (x >= 0 and y >= 5 * x)) #FORWARD SECTION
+            or ((x < 0 and y <= 5 * x) or (x >= 0 and y <= -5 * x))): #BACKWARD SECTION
+            self.set_same_speed(speed)
+        else:
+            self.set_diff_speed(speed, x)
+            
+        #DATA SENDING
+        self.send_data()
+        
+        #LOGS PRINTING
+        if (x < 0 and y >= 0.2 * x and y <= -0.2 * x):
+            self.get_logger().info("ROTATE RIGHT")
+        elif (x >= 0 and y <= 0.2 * x and y >= -0.2 * x):
+            self.get_logger().info("ROTATE LEFT")
+        elif ((x < 0 and y >= -5 * x) or (x >= 0 and y >= 5 * x)):
+            self.get_logger().info("GO FORWARD")
+        elif ((x < 0 and y <= 5 * x) or (x >= 0 and y <= -5 * x)):
+            self.get_logger().info("GO BACKWARD")
+        elif (x < 0 and y <= -5 * x and y >= -0.2 * x):
+            self.get_logger().info("DIFFERENTIAL FRONT RIGHT")
+        elif (x < 0 and y <= 0.2 * x and y >= 5 * x):
+            self.get_logger().info("DIFFERENTIAL BACK RIGHT")
+        elif (x > 0 and y >= 0.2 * x and y <= 5 * x):
+            self.get_logger().info("DIFFERENTIAL FRONT LEFT")
+        elif (x > 0 and y <= -0.2 * x and y >= -5 * x):
+            self.get_logger().info("DIFFERENTIAL BACK LEFT")
+        else:
+            self.get_logger().info("Not moving")
+        
+        self.get_logger().info(f"SPEED: {round(speed * 255)}")
+        
+    def finish_step(self):
+        msg = String()
+        msg.data = "OK"
+        self.publisher.publish(msg)
+        self.curr_rotation = 0
+        self.curr_distance = 0
+        
+    def send_data(self):
+        self.ser.write(self.lf_speed.to_bytes(1, 'little'))
+        self.ser.write(self.lf_direction.to_bytes(1, 'little')) #FRONT LEFT WHEEL
+        
+        self.ser.write(self.rf_speed.to_bytes(1, 'little'))
+        self.ser.write(self.rf_direction.to_bytes(1, 'little')) #FRONT RIGHT WHEEL
+        
+        self.ser.write(self.rb_speed.to_bytes(1, 'little'))
+        self.ser.write(self.rb_direction.to_bytes(1, 'little')) #BACK RIGHT WHEEL
+        
+        self.ser.write(self.lb_speed.to_bytes(1, 'little'))
+        self.ser.write(self.lb_direction.to_bytes(1, 'little')) #BACK LEFT WHEEL
+        
+    def set_ahead(self):
+        self.rf_direction = 255
+        self.lf_direction = 1
+        self.rb_direction = 255
+        self.lb_direction = 1
+        
+    def set_backward(self):
+        self.rf_direction = 1
+        self.lf_direction = 255
+        self.rb_direction = 1
+        self.lb_direction = 255
+    
+    def set_right(self):
+        self.rf_direction = 1
+        self.lf_direction = 1
+        self.rb_direction = 1
+        self.lb_direction = 1
+        
+    def set_left(self):
+        self.rf_direction = 255
+        self.lf_direction = 255
+        self.rb_direction = 255
+        self.lb_direction = 255
+        
+    def set_same_speed(self, speed):
+        """
+        Sets the same speed to all 4 wheels 
+
+        Args:
+            speed (float): speed value between 0 and 1
+        """
+        weighted_speed = round(speed * 255)
+        
+        self.rf_speed = weighted_speed
+        self.lf_speed = weighted_speed
+        self.rb_speed = weighted_speed
+        self.lb_speed = weighted_speed
+        
+    def set_diff_speed(self, speed, x):
+        """
+        Sets differential speed to the wheels depending on 
+        which side we want to turn to
+
+        Args:
+            speed (float): absolute speed value to be used for 
+                            differential computations
+            x (float): horizontal value of the left joystick
+        """
+        weighted_speed = round(speed * 255) #max speed is 255
+        
+        #the inside the turn wheels' speeds are multiplied by a 
+        # factor (1 - abs(x)) bounded by 0 and 1, meaning they will 
+        # be slowed the more sharply we want to turn, and an additional
+        # factor to slow even more the speed of the wheels
+        diff_weighted_speed = round(speed * 255 * (1 - abs(x)) * 0.5)
+        
+        if x > 0: #turn left
+            self.rf_speed = weighted_speed
+            self.rb_speed = weighted_speed
+            self.lf_speed = diff_weighted_speed
+            self.lb_speed = diff_weighted_speed
+        else: #turn right
+            self.rf_speed = diff_weighted_speed
+            self.rb_speed = diff_weighted_speed
+            self.lf_speed = weighted_speed
+            self.lb_speed = weighted_speed
+        
+        
+def main(args=None):
+    rclpy.init(args=args)
+    node = TrajectorySubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
